89abcd2aaca32d08513864df4c70cd90a7f9310b
Nikita <norenkonikita@gmail.com>
2021-08-18 19:19:57 +0300

    stlyes, scripts, fonts set up

--- functions.php
+++ functions.php
@@ -3,12 +3,10 @@
 
 function scripts()
 {
-    wp_register_style( 'style', get_template_directory_uri().'/dist/app.css', [], 1, 'all');
+    wp_register_style( 'style', get_template_directory_uri().'/style.min.css', [], 1, 'all');
     wp_enqueue_style('style');
-
     
-
-    wp_register_script( 'app', get_template_directory_uri().'/dist/app.js', [], 1, true);
+    wp_register_script( 'app', get_template_directory_uri().'/assets/js/custom.min.js', [], 1, true);
     wp_enqueue_script('app');
 }
 add_action( 'wp_enqueue_scripts', 'scripts');
