2e55aa1aa217a13e03609bada91f31f076d53313
nickwhoa <norenkonikita@gmail.com>
2021-08-22 23:13:08 +0300

    block 2. done

--- page-benefits.php
+++ page-benefits.php
@@ -1,6 +1,46 @@
 <main>
 <section id='benefits' class="benefits">
-    <div class="flex-container">
-        
-    </div>
+    
+        <div class="benefits__title">
+            <h2>ЧИ ПІДХОДИТЬ ВАМ ЦЕЙ КУРС?</h2>
+            <div class="subtitle flex-container">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_on.svg" alt="Знайдіть пункти, які вам підходять">
+                Знайдіть пункти, які вам підходять
+            </div>
+        </div>  
+        <div class="benefits__dash-line">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/dash-line.png" alt="">
+        </div>
+        <div class="benefits__list flex-container">
+            <div class="benefits-item">
+                <div class="benefits-item__title"> <span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_side_r.svg" alt=""></span> Я мебляр-початківець</div>
+                <div class="benefits-item__description">
+                і не знаю, з чого почати, щоб вийти на новий рівень і масштабуватися
+                </div>
+            </div>
+            <div class="benefits-item">
+                <div class="benefits-item__title"> <span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_side_r.svg" alt=""></span> Я мебляр-початківець</div>
+                <div class="benefits-item__description">
+                і не знаю, з чого почати, щоб вийти на новий рівень і масштабуватися
+                </div>
+            </div>
+            <div class="benefits-item">
+                <div class="benefits-item__title"> <span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_side_r.svg" alt=""></span> Я мебляр-початківець</div>
+                <div class="benefits-item__description">
+                і не знаю, з чого почати, щоб вийти на новий рівень і масштабуватися
+                </div>
+            </div>
+            <div class="benefits-item">
+                <div class="benefits-item__title"> <span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_side_r.svg" alt=""></span> Я мебляр-початківець</div>
+                <div class="benefits-item__description">
+                і не знаю, з чого почати, щоб вийти на новий рівень і масштабуватися
+                </div>
+            </div>
+            <div class="benefits-item">
+                <div class="benefits-item__title"> <span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/view_side_r.svg" alt=""></span> Я мебляр-початківець</div>
+                <div class="benefits-item__description">
+                і не знаю, з чого почати, щоб вийти на новий рівень і масштабуватися
+                </div>
+            </div>
+        </div>
 </section>
