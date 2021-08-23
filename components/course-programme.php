858cafa01733bd6333f46dee877569f5cfaee91e
Nikita <norenkonikita@gmail.com>
2021-08-23 17:31:18 +0300

    block 4. course-programme. done with points

---
+++ components/course-programme.php
@@ -0,0 +1,104 @@
+<section id="course-programme" class="course-programme">
+    <!-- TODO: COURSE LINE -->
+    <div class="container">
+        <div class="course-programme__title">
+            <h2>ПРОГРАМА КУРСУ</h2>
+        </div>
+        <div class="course-programme__download">
+            <a href="#" class="link">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/download-icon.png" alt="Скачать презентацию">Скачать презентацию
+            </a>
+        </div>
+        <div class="course-programme__modules">
+
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">1</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+                    <ul>
+                        <li>Стратегія</li>
+                        <li>Договір: оформлення відносин з клієнтами</li>
+                        <li>Основи фінансів для бізнесу</li>
+                        <li>Тайм-менеджмент</li>
+                        <li>Побудова команди: підбір персоналу, інструменти мотивації співробітників</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">2</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+                    <ul>
+                        <li>Стратегія</li>
+                        <li>Договір: оформлення відносин з клієнтами</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">3</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+                    <ul>
+                        <li>Стратегія</li>
+                        <li>Договір: оформлення відносин з клієнтами</li>
+                        <li>Основи фінансів для бізнесу</li>
+                        <li>Тайм-менеджмент</li>
+                        <li>Побудова команди: підбір персоналу, інструменти мотивації співробітників</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">4</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+
+                </div>
+            </div>
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">5</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+                    <ul>
+                        <li>Стратегія</li>
+                        <li>Договір: оформлення відносин з клієнтами</li>
+                        <li>Основи фінансів для бізнесу</li>
+                        <li>Тайм-менеджмент</li>
+                        <li>Побудова команди: підбір персоналу, інструменти мотивації співробітників</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="module">
+                <div class="module__header flex-container">
+                    <div class="circle"></div>
+                    <div class="number">6</div>
+                    <div class="ext">модуль</div>
+                    <div class="title">Ефективне управління проектом</div>
+                </div>
+                <div class="module__content">
+
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</section>
--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
 <?php get_template_part( 'components/approaches' ); ?>
 <?php get_template_part( 'components/benefits' ); ?>
+<?php get_template_part( 'components/course-programme' ); ?>
 <?php get_footer(); ?>
