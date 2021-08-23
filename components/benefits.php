f10be889d7bcac1ded9cd69d39462a437a5328c6
Nikita <norenkonikita@gmail.com>
2021-08-23 15:41:34 +0300

    block 3. benefits. done

---
+++ components/benefits.php
@@ -0,0 +1,55 @@
+<section class="benefits">
+    <div class="benefits__title" >
+        <h2>ПЕРЕВАГИ</h2>
+    </div>
+    <div class="benefits__list flex-container" id="benefits">
+        <div class="benefit-item">
+            <div class="benefit-item__feature-image">
+                <div class="image-wrapper">
+                    <img src="https://image.flaticon.com/icons/png/512/616/616578.png" alt="Перевага">
+                </div>
+            </div>
+            <div class="benefit-item__divider"></div>
+            <div class="benefit-item__title">Усі базові напрями в одному курсі</div>
+            <div class="benefit-item__description">
+                управління, сервіс, продажі, дизайн, маркетинг, робота з Viyar Bazar. Після курсу у вас буде комплекс знань, щоб будувати команду та просувати послуги
+            </div>
+        </div>
+        <div class="benefit-item">
+            <div class="benefit-item__feature-image">
+                <div class="image-wrapper">
+                    <img src="https://image.flaticon.com/icons/png/512/616/616578.png" alt="Перевага">
+                </div>
+            </div>
+            <div class="benefit-item__divider"></div>
+            <div class="benefit-item__title">Усі базові напрями в одному курсі</div>
+            <div class="benefit-item__description">
+                управління, сервіс, продажі, дизайн, маркетинг, робота з Viyar Bazar. Після курсу у вас буде комплекс знань, щоб будувати команду та просувати послуги
+            </div>
+        </div>
+        <div class="benefit-item">
+            <div class="benefit-item__feature-image">
+                <div class="image-wrapper">
+                    <img src="https://image.flaticon.com/icons/png/512/616/616578.png" alt="Перевага">
+                </div>
+            </div>
+            <div class="benefit-item__divider"></div>
+            <div class="benefit-item__title">Усі базові напрями в одному курсі</div>
+            <div class="benefit-item__description">
+                управління, сервіс, продажі, дизайн, маркетинг, робота з Viyar Bazar. Після курсу у вас буде комплекс знань, щоб будувати команду та просувати послуги
+            </div>
+        </div>
+        <div class="benefit-item">
+            <div class="benefit-item__feature-image">
+                <div class="image-wrapper">
+                    <img src="https://image.flaticon.com/icons/png/512/616/616578.png" alt="Перевага">
+                </div>
+            </div>
+            <div class="benefit-item__divider"></div>
+            <div class="benefit-item__title">Усі базові напрями в одному курсі</div>
+            <div class="benefit-item__description">
+            для закріплення знань даємо завдання, які викладачі особисто перевіряють
+            </div>
+        </div>
+    </div>
+</section>
