0e5b28d19a97833819b26770cb8d54badb88a572
Nikita <norenkonikita@gmail.com>
2021-09-06 19:02:36 +0300

    contacts & faq

--- components/contacts.php
+++ components/contacts.php
@@ -1,13 +1,69 @@
 </main>
 <address class="contacts">
     <div class="contacts-wrapper">
-    <div class="contacts__info">
-        <div class="title">
-            <h2>
-            КОНТАКТИ
-            </h2>
+        <div class="contacts__info">
+            <div class="title">
+                <h2>
+                    КОНТАКТИ
+                </h2>
+            </div>
+            <div class="phone">
+                <header>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M27.3216 21.6136L23.9723 18.2643C22.7761 17.0681 20.7427 17.5467 20.2642 19.1016C19.9053 20.1782 18.7092 20.7763 17.6326 20.537C15.2403 19.9389 12.0106 16.8289 11.4125 14.317C11.0537 13.2404 11.7714 12.0442 12.8479 11.6854C14.403 11.2069 14.8814 9.17343 13.6853 7.97726L10.336 4.62799C9.37906 3.79067 7.94366 3.79067 7.10634 4.62799L4.83362 6.90071C2.5609 9.29305 5.07285 15.6327 10.6948 21.2547C16.3168 26.8767 22.6565 29.5083 25.0489 27.116L27.3216 24.8432C28.1589 23.8863 28.1589 22.4509 27.3216 21.6136Z" fill="white" />
+                    </svg>
+                    Телефон:
+                </header>
+                <a href="tel:+380978929786">+380 (97) 892 97 86</a>
+            </div>
+            <div class="mail">
+                <header>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.9091 16.373L28.0001 21.488V11.042L19.9091 16.373Z" fill="white" />
+                        <path d="M4 11.042V21.488L12.091 16.373L4 11.042Z" fill="white" />
+                        <path d="M26.4999 8H5.49992C4.75142 8 4.15742 8.558 4.04492 9.2765L15.9999 17.153L27.9549 9.2765C27.8424 8.558 27.2484 8 26.4999 8Z" fill="white" />
+                        <path d="M18.535 17.2788L16.4125 18.6768C16.2865 18.7593 16.144 18.7998 16 18.7998C15.856 18.7998 15.7135 18.7593 15.5875 18.6768L13.465 17.2773L4.04797 23.2338C4.16347 23.9463 4.75447 24.4998 5.49997 24.4998H26.5C27.2455 24.4998 27.8365 23.9463 27.952 23.2338L18.535 17.2788Z" fill="white" />
+                    </svg>
+                    E-mail:
+                </header>
+                <a href="mailto:academyviyar@gmail.com">academyviyar@gmail.com</a>
+            </div>
+            <div class="address">
+                <header>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16 26C16 26 11.8 21.5 10 18C9.1 16.2 8 14.1 8 12C8 7.6 11.6 4 16 4C20.4 4 24 7.6 24 12C24 14.1 22.9 16.2 22 18C20.2 21.5 16 26 16 26ZM16 9C14.3 9 13 10.3 13 12C13 13.7 14.3 15 16 15C17.7 15 19 13.7 19 12C19 10.3 17.7 9 16 9Z" fill="white" />
+                    </svg>
+                    Адреса: 
+                </header>
+                м. Київ, б-р Вацлава Гавела, 16
+            </div>
+            <div class="offer">
+                <header>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18.8 6C18.4 4.8 17.3 4 16 4C14.7 4 13.6 4.8 13.2 6H6V26H26V6H18.8ZM24 24H8V8H15V7C15 6.4 15.4 6 16 6C16.6 6 17 6.4 17 7V8H24V24ZM12 21H10V19H12V21ZM12 17H10V15H12V17ZM12 13H10V11H12V13ZM22 21H14V19H22V21ZM22 17H14V15H22V17ZM22 13H14V11H22V13Z" fill="white" />
+                    </svg>
+                    <span>Договір оферти</span>
+                </header>
+            </div>
+            <div class="speaker">
+                <header>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16 2C8.3 2 2 8.3 2 16C2 23.7 8.3 30 16 30C23.7 30 30 23.7 30 16C30 8.3 23.7 2 16 2ZM12 10C12 7.8 13.8 6 16 6C18.2 6 20 7.8 20 10C20 10.2 20 10.8 20 11C20 13.8 18.2 17 16 17C13.8 17 12 13.8 12 11C12 10.8 12 10.2 12 10ZM16 28C11.3 28 7.2 25.3 5.3 21.3C5.4 21.1 5.5 20.9 5.7 20.8C7.4 19.3 9.4 18.2 11.7 17.6L16 22L20.4 17.6C22.6 18.2 24.6 19.3 26.4 20.8C26.6 20.9 26.7 21.1 26.8 21.3C24.8 25.3 20.7 28 16 28Z" fill="white" />
+                    </svg>
+                    Стати спікером
+                </header>
+                <a href="mailto:academyviyar@gmail.com">academyviyar@gmail.com</a>
+            </div>
+            <div class="register register-btn-wrapper">
+                <a href="#" class="link">
+                    <button class="register-btn btn">Зареєструватися</button>
+                </a>
+            </div>
         </div>
-    </div>
-    <div class="contacts__map"></div>
+        <div class="contacts__map">
+            <div class="map-wrapper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d449.1948189782809!2d30.407754044209728!3d50.44054893012331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cc72b04e87e1%3A0x16d3081047d71620!2sVatslava%20Havela%20Blvd%2C%20Kyiv%2C%2002000!5e0!3m2!1sen!2sua!4v1630930804824!5m2!1sen!2sua" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
     </div>
 </address>
