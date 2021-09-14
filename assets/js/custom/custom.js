6b7414325d778d03d8827fd5dbcfb164eb65670b
nickwhoa <norenkonikita@gmail.com>
2021-09-14 03:26:56 +0300

    smooth scroll

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -185,6 +185,14 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 });
 
